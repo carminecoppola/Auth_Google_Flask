9599a7f4a9f09d69a9e9f3b0ce17ecd4df762e4e
carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-07-25 18:16:41 +0200

    FindMaxAndMin

---
+++ EserciziBanali/Istogramm.py
@@ -0,0 +1,30 @@
+# Scrivi una semplice funzione che, data una lista di numeri, fornisca in output un
+# istogramma basato su questi numeri, usando asterischi per disegnarlo.
+# Data ad esempio la lista [3, 7, 9, 5], la funzione dovrà produrre questa sequenza:
+#   ***
+#   *******
+#   *********
+#   *****
+
+from random import randint
+
+
+def istogramma(lista):
+    for i in lista:
+        print('*' * i)
+
+
+def main():
+    list_num = []
+
+    for i in range(4):
+        list_num.append(randint(1, 10))
+
+    print("\nEcco la lista: ", list_num)
+
+    print("\nRappresentazione tramite istogramma:")
+    istogramma(list_num)
+
+
+if __name__ == '__main__':
+    main()
